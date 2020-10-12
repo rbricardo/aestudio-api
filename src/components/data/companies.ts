8be23021d347e203a384a45eba60babbc372b178
Ricardo Barbosa <rb.ricardo01@gmail.com>
2020-10-12 18:46:25 -0300

    refactor: export company interface

--- src/components/data/companies.ts
+++ src/components/data/companies.ts
@@ -1,5 +1,21 @@
 import axios from 'axios'
 
+export interface Company {
+  business_name: string
+  city: string
+  location_1: {
+    latitude: string
+    longitude: string
+  }
+  location_account: string
+  location_description: string
+  location_start_date: string
+  naics: string
+  primary_naics_description: string
+  street_address: string
+  zip_code: string
+}
+
 export const getCompanies = async (): Promise<any> => {
   try {
     const { data } = await axios.get('https://data.lacity.org/resource/6rrh-rzua.json')
